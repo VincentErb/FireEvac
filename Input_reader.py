db16aa07ee36ce154e2beca1e45442dc3b1e76fd
Vincent Erb <vincent.erb@hotmail.fr>
2019-05-16 23:37:40 +0200

    input_reader ok, with dicos and solution reader

--- Input_reader.py
+++ Input_reader.py
@@ -1,106 +1,125 @@
-import numpy as np
+# Function that returns two python dictionnaries : evac_paths & arcs
+def parse_instance(path):
 
+    evac_paths = {}  # {node_id : {'pop', 'max_rate', 'route_length', 'route_nodes':[node1,node2,..]} }
+    arcs = {}  # arcs = {(node1,node2):{'duedate', 'length', 'capacity'}}
 
-# Object that describes the parameters of an arc (used in arc matrix)
-class ArcParameters:
-    def __init__(self, duedate, length, capacity):
-        self.duedate = duedate
-        self.length = length
-        self.capacity = capacity
+    myfile = open(path, 'r')
+    ins = myfile.readlines()
+    i = 0
+    state = 0
 
+    while i < len(ins):
+        l = ins[i]
+        l = l.split(" ")
 
-class InputData:
-    def __init__(self):
-        self.num_evac_nodes = None
-        self.safe_node = None
-        self.num_nodes = None
-        self.num_arcs = None
-        self.evac_info = None
-        self.arcs = None
+        if l[0] == 'c':
+            i = i + 1
 
-    def parsedata(self, path):
-        myfile = open(path, 'r')
-        ins = myfile.readlines()
-        i = 0
-        state = 0
+        elif state == 0:
+            num_evac_nodes = int(l[0])
+            # safe_node = int(l[1]) UNUSED RIGHT NOW, MAY BE USEFUL LATER
 
-        while i < len(ins):
-            l = ins[i]
-            l = l.split(" ")
-
-            if l[0] == 'c':
+            for j in range(0, num_evac_nodes):
                 i = i + 1
+                l = ins[i]
+                l = l.split(" ")
+                node_id = int(l[0])  # id of node
+                pop = int(l[1])  # population
+                max_rate = int(l[2])  # max rate
+                route_length = int(l[3])  # k : length of route
+                route_nodes = []  # escape route nodes init as empty set
 
-            elif state == 0:
-                self.num_evac_nodes = int(l[0])
-                self.safe_node = int(l[1])
-                self.evac_info = np.empty([int(self.num_evac_nodes), 5], dtype=object)
+                for k in range(0, int(l[3])):
+                    route_nodes.append(int(l[4 + k]))
 
-                for j in range(0, int(self.num_evac_nodes)):
-                    i = i + 1
-                    l = ins[i]
-                    l = l.split(" ")
-                    self.evac_info[j, 0] = int(l[0])                    # id of node
-                    self.evac_info[j, 1] = int(l[1])                    # population
-                    self.evac_info[j, 2] = int(l[2])                    # max rate
-                    self.evac_info[j, 3] = int(l[3])                    # k : length of route
-                    self.evac_info[j, 4] = np.empty([int(l[3])])     # escape route nodes
+                evac_paths[node_id] = {'pop': pop, 'max_rate': max_rate, 'route_length': route_length,
+                                       'route_nodes': route_nodes}
 
-                    for k in range(0, int(l[3])):
-                        self.evac_info[j, 4][k] = int(l[4 + k])
+            i = i + 1
+            state = 1
 
+        elif state == 1:
+            # num_nodes = int(l[0]) UNUSED RIGHT NOW, MAY BE USEFUL LATER
+            num_arcs = int(l[1])
+
+            for j in range(0, num_arcs):
                 i = i + 1
-                state = 1
+                l = ins[i]
+                l = l.split(" ")
+                node1 = int(l[0])
+                node2 = int(l[1])
+                duedate = int(l[2])
+                length = int(l[3])
+                capacity = int(l[4])
 
-            elif state == 1:
-                self.num_nodes = int(l[0])
-                self.num_arcs = int(l[1])
-                self.arcs = np.empty([int(self.num_nodes), self.num_nodes], dtype=object)
+                if node1 < node2:
+                    arcs[(node1, node2)] = {'due_date': duedate, 'length': length, 'capacity': capacity}
+                else:
+                    arcs[(node2, node1)] = {'due_date': duedate, 'length': length, 'capacity': capacity}
 
-                for j in range(0, int(self.num_arcs)):
-                    i = i + 1
-                    l = ins[i]
-                    l = l.split(" ")
-                    tmp = ArcParameters(int(l[2]), float(l[3]), float(l[4]))
-                    self.arcs[int(l[0]), int(l[1])] = tmp
+            state = -1
 
-                state = -1
+        else:
+            i = i + 1
 
-            else:
-                i = i + 1
-
-    def print_input_data(self):
-
-        print("========================")
-        print("INFORMATION D'EVACUATION")
-        print("========================")
-        print("Nombre de noeuds à évacuer : " + str(self.num_evac_nodes))
-        print("Sortie : " + str(self.safe_node))
-        print("----------")
-        for i in range(0, self.num_evac_nodes):
-            s = "Trajet : " + str(self.evac_info[i][0])
-            for j in range(0, self.evac_info[i][3]):
-                s = s + " - " + str(self.evac_info[i][4][j])
-
-            s = s + " || Population : " + str(self.evac_info[i][1]) + " || Taux d'évacuation max : " \
-                + str(self.evac_info[i][2])
-            print(s)
-
-        print("")
-        print("==============")
-        print("ARCS DU GRAPHE")
-        print("==============")
-        print("Nombre de noeuds : " + str(self.num_nodes))
-        print("Nombre d'arcs : " + str(self.num_arcs))
-        print("-----------------")
-        for i in range(0, self.num_nodes):
-            for j in range(0, self.num_nodes):
-                if self.arcs[i, j] is not None:
-                    print("(" + str(i) + "," + str(j) + ") - Duedate : " + str(self.arcs[i, j].duedate)
-                          + " || Length : " + str(self.arcs[i, j].length) + " || Capacity : "
-                          + str(self.arcs[i, j].capacity))
+    return evac_paths, arcs
 
 
-test = InputData()
-test.parsedata('./Exemple/graphe-TD-sans-DL-data.txt')
-test.print_input_data()
+# Function that takes as input the two dictionnaries (evac_paths + arcs) and prints them
+def print_input_data(evac_paths, arcs):
+    print("========================")
+    print("INFORMATION D'EVACUATION")
+    print("========================")
+    print("Nombre de noeuds à évacuer : " + str(len(evac_paths)))
+    print("----------")
+    for keys, values in evac_path.items():
+        print(keys)
+        print(values)
+
+    print("")
+    print("==============")
+    print("ARCS DU GRAPHE")
+    print("==============")
+    print("Nombre d'arcs : " + str(len(arcs)))
+    print("-----------------")
+    for keys, values in arcs.items():
+        print(keys)
+        print(values)
+
+
+# Parses solution data
+def parse_solution(path):
+
+    solution = {}  # {'instance_name', 'nb_evac_nodes', 'node_data', 'validity', 'aim_function', 'time', 'method'}
+
+    myfile = open(path, 'r')
+    l = myfile.readline()
+    solution['instance_name'] = l.rstrip('\n')
+    l = myfile.readline()
+    solution['nb_evac_nodes'] = int(l)
+    node_data = {}  # {'id', evac_rate, 'start'}
+
+    for i in range(solution['nb_evac_nodes']):
+        l = myfile.readline()
+        ins = l.split()
+        node_data[int(ins[0])] = (int(ins[1]), int(ins[2]))
+
+    solution['node_data'] = node_data
+
+    l = myfile.readline()
+    solution['validity'] = l.rstrip('\n')
+    l = myfile.readline()
+    solution['aim_function'] = int(l)
+    l = myfile.readline()
+    solution['time'] = int(l)
+    l = myfile.readline()
+    solution['method'] = l.rstrip('\n')
+
+    return solution
+
+
+evac_path, arc = parse_instance('./Exemple/graphe-TD-sans-DL-data.txt')
+print_input_data(evac_path, arc)
+sol = parse_solution('./Exemple/graphe-TD-sans-DL-sol.txt')
+print(sol)
