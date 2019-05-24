b2674a8491c424bfe4944f5d0b2c9185738c0a83
Vincent Erb <vincent.erb@hotmail.fr>
2019-05-24 14:05:00 +0200

    checker done, needs further testing

--- Checker.py
+++ Checker.py
@@ -36,42 +36,117 @@ def check_max_rate(solution, evac_paths, arcs):
 # Creates gantt blocks from evac data, solution data, and arcs from input data
 # To be used in capacity check
 def create_gantt(solution, evac_paths, arcs):
-    gantt = {}                                       # {'start_node', ('n1', 'n2'): 'evac_rate', 'start', 'end', 'tail'}
+    gantt = {}                                                          # {'start_node', ('n1', 'n2'): 'evac_rate', 'start'}
     solution_nodes = solution['node_data']
 
     for node_id, values in evac_paths.items():
         prev = node_id
         (evac_rate, start) = solution_nodes[node_id]
-        end = 0
         for current in values['route_nodes']:
 
             if prev < current:
-                end = start + (values['pop'] // arcs[(prev, current)]['capacity'])
-                gantt[(node_id, (prev, current))] = (evac_rate, start, end)
+                gantt[(node_id, (prev, current))] = (evac_rate, start)
                 start = start + arcs[(prev, current)]['length']
-
             else:
-                end = start + (values['pop'] // arcs[(current, prev)]['capacity'])
-                gantt[(node_id, (prev, current))] = (evac_rate, start, end)
+                gantt[(node_id, (prev, current))] = (evac_rate, start)
                 start = start + arcs[(current, prev)]['length']
-
             prev = current
 
-        end = -1
-        gantt[(node_id, (prev, 'done'))] = (evac_rate, start, end)
+        gantt[(node_id, (prev, 'done'))] = (evac_rate, start)
 
-    print(gantt)
+    # print(gantt)
     return gantt
 
 
+def calculate_objective(evac_nodes, gantt):
+    endlist = []
+    for key in gantt:
+        if key[1][1] == 'done':
+            st = gantt[key][1]
+            pop = evac_nodes[int(key[0])]['pop']
+            rate = evac_nodes[int(key[0])]['max_rate']
+            tmp = pop % rate
+            if tmp == 0:
+                endlist.append(st + pop // rate)
+            else:
+                endlist.append(st + (pop // rate) + 1)
+
+    return max(endlist)
+
+
 # Verifies capacity with gantt
-def check_capacity():
-    print("coucou")
+def check_capacity(evac_nodes, arcs, gantt):
+    valid = True
+
+    relevent_arcs = set()
+
+    for key in gantt:
+        if key[1][1] != 'done':
+            relevent_arcs.add(key[1])
+
+    for i in relevent_arcs:
+        cap = arcs[i]['capacity']
+        info = []
+        for k in gantt:
+            if k[1] == i:
+                if evac_nodes[k[0]]['pop'] % gantt[k][0] == 0:
+                    info.append([gantt[k][0], gantt[k][1], evac_nodes[k[0]]['pop'] // gantt[k][0], gantt[k][0]])
+                else:
+                    info.append([gantt[k][0], gantt[k][1], (evac_nodes[k[0]]['pop'] // gantt[k][0]) + 1, evac_nodes[k[0]]['pop'] % gantt[k][0]])
+        # print(cap)
+        # print(info)
+
+        start = min(info)[1]
+        end = max([s+d for (c, s, d, l) in info])
+
+        # print(start)
+        # print(end)
+
+        # For every time unit
+        for j in range(start, end):
+            current_pers = 0
+            for (ra, st, ft, last) in info:
+                if st <= j < st + ft - 1:
+                    current_pers = current_pers + ra
+                # GESTION DERNIER BLOC
+                elif j == st + ft - 1:
+                    current_pers = current_pers + last
+            if current_pers > cap:
+                print(i, "problem capacity =", cap)
+                valid = False
+
+        return valid
 
 
-evac, ark = ir.parse_instance('./Exemple/graphe-TD-sans-DL-data.txt')
-sol = ir.parse_solution('./Exemple/graphe-TD-sans-DL-sol.txt')
+def test():
+    evac, ark = ir.parse_instance('./Exemple/graphe-TD-sans-DL-data.txt')
+    sol = ir.parse_solution('./Exemple/graphe-TD-sans-DL-sol.txt')
 
-print(check_max_rate(sol, evac, ark))
+    print(check_max_rate(sol, evac, ark))
 
-create_gantt(sol, evac, ark)
+    g = create_gantt(sol, evac, ark)
+    # print(calculate_objective(evac, g))
+    print(check_capacity(evac, ark, g))
+
+
+def run(instance_path, solution_path):
+    evac, ark = ir.parse_instance(instance_path)
+    sol = ir.parse_solution(solution_path)
+    print("Checking max rate ... ", end='')
+    if check_max_rate(sol,evac,ark):
+        print("OK")
+    else:
+        print("FAILED")
+    print("Checking capacity ... ", end='')
+
+    g = create_gantt(sol, evac, ark)
+    if check_capacity(evac, ark, g):
+        print("OK")
+    else:
+        print("FAILED")
+
+    print("Objective function value : ", end='')
+    print(calculate_objective(evac, g))
+
+
+run('./Exemple/graphe-TD-sans-DL-data.txt', './Exemple/graphe-TD-sans-DL-sol.txt')
