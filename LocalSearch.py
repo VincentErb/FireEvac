fd9bda4a355af6f6c9d8970b02bc10d502144843
Arens <maxime.arens@gmail.com>
2019-06-05 16:23:20 +0200

    correcting localsearch

--- LocalSearch.py
+++ LocalSearch.py
@@ -1,8 +1,9 @@
 import FireEvac.Input_reader as ir
 import FireEvac.Checker as chk
+import FireEvac.Bornes as brn
 import random as rd
 
-def local_search(solution):
+def local_search(instance_path, solution):
     nb_accept = 5
     cpt_validity = 0 # goes from 0 to nb_accept to allow non-valid solution for a time
     value = 10000
@@ -12,13 +13,16 @@ def local_search(solution):
         modif_table = {}
         better_solution = solution
         for i in range(10):
-            a = rd.randint(0,solution['nb_evac_nodes']+1)
+            a = rd.randint(1,solution['nb_evac_nodes'])
             b = rd.randint(0,4)
-            new_solution = modify_solution(solution, a, b) # we modify one paramater of one path
-            if check_better_solution > check_new_solution() :
+            new_solution = modify_solution(solution, a, b)  # we modify one paramater of one path
+            valid, check_new_solution = chk.run_with_objective(instance_path, new_solution)
+            v, check_better_solution = chk.run_with_objective(instance_path, better_solution)
+            if check_better_solution > check_new_solution:
                 if valid:
                     cpt_validity = 0
                     better_solution = new_solution
+                    check_better_solution = check_new_solution
                 else:
                     if cpt_validity < nb_accept :
                         better_solution = new_solution
@@ -26,33 +30,34 @@ def local_search(solution):
 
 
 
-        modif_table[ite]= { 'solution' : better_solution,'result' : result, 'validity' : validity}
+        modif_table[ite]= { 'solution' : better_solution,'result' : check_better_solution, 'validity' : valid}
         solution = better_solution
 
-    for keys, values in modif_table.items() :
-        if value > values['result'] and values['validity'] == 0:
+    for keys, values in modif_table.items():
+        if value > values['result'] and values['validity'] == True:
             new_solution = values['solution']
             value = values['result']
+    print(value)
 
 
     return new_solution
 
 def modify_solution(solution, path_to_modify, modification):
     # modification : 0 -> -1 on evac_rate, 1 -> +1 on evac_rate, 2 -> -1 on start 3-> +1 on start
-    i = 0
-    for values in solution.items():
-        if i == path_to_modify :
-            (a, b) = values['node_data']
-            if modification == 0:
-                if a != 0 :
-                    (a, b) = (a - 1, b)
-            elif modification == 1:
-                (a, b) = (a + 1, b)
-            elif modification == 2:
-                if b != 0 :
-                    (a,b) = (a, b - 1)
-            elif modification == 3:
-                (a, b) = (a, b + 1)
-            values['node_data'] = (a, b)
-        i = i + 1
+    a, b = solution['node_data'][path_to_modify]
+    if modification == 0:
+        if a != 0 :
+            (a, b) = (a - 1, b)
+    elif modification == 1:
+        (a, b) = (a + 1, b)
+    elif modification == 2:
+        if b != 0 :
+            (a,b) = (a, b - 1)
+    elif modification == 3:
+        (a, b) = (a, b + 1)
+    solution['node_data'][path_to_modify] = (a, b)
     return solution
+
+evac, ark = ir.parse_instance('./Exemple/graphe-TD-sans-DL-data.txt')
+local_search('./Exemple/graphe-TD-sans-DL-data.txt', brn.borne_sup_solution(evac, ark))
+
