4298418a0eb085caafa8aeca68202df93d54cd12
Arens <maxime.arens@gmail.com>
2019-06-06 15:38:04 +0200

    changing parameters

--- LocalSearch.py
+++ LocalSearch.py
@@ -5,7 +5,7 @@ from copy import deepcopy
 import random as rd
 
 
-def local_search(instance_path, evac_path, solution):
+def local_search(instance_path, evac_path, arcs, solution):
     nb_accept = 50
     cpt_validity = 0 # goes from 0 to nb_accept to allow non-valid solution for a time
     ite = 10000
@@ -18,29 +18,32 @@ def local_search(instance_path, evac_path, solution):
     print(list_start_node)
     while ite > 0:
         ite = ite - 1
-        for i in range(20):
-            a = rd.randint(0, len(list_start_node)-1)
-            b = rd.randint(0, 3)
-            v, check_better_solution = chk.run_with_objective(instance_path, better_solution)
-            new_solution = modify_solution(better_solution, list_start_node[a], b)  # we modify one paramater of one path
-            valid, check_new_solution = chk.run_with_objective(instance_path, new_solution)
-            print("new", chk.run_with_objective(instance_path, new_solution))
-            print("better", chk.run_with_objective(instance_path, better_solution))
-            if check_better_solution >= check_new_solution:
-                if valid:
-                    cpt_validity = 0
-                    better_solution = new_solution
-                    modif_true[0] = better_solution
-                    modif_true[1] = check_new_solution
-                else:
-                    if cpt_validity < nb_accept:
+        if modif_true[1] == brn.borne_inf(evac_path, arcs):
+            break
+        else:
+            for i in range(20):
+                a = rd.randint(0, len(list_start_node)-1)
+                b = rd.randint(0, 3)
+                v, check_better_solution = chk.run_with_objective(instance_path, better_solution)
+                new_solution = modify_solution(better_solution, list_start_node[a], b)  # we modify one paramater of one path
+                valid, check_new_solution = chk.run_with_objective(instance_path, new_solution)
+                print("new", chk.run_with_objective(instance_path, new_solution))
+                print("better", chk.run_with_objective(instance_path, better_solution))
+                if check_better_solution >= check_new_solution:
+                    if valid:
+                        cpt_validity = 0
                         better_solution = new_solution
-                        cpt_validity = cpt_validity + 1
-                        modif_false[0] = better_solution
-                        modif_false[1] = check_new_solution
+                        modif_true[0] = better_solution
+                        modif_true[1] = check_new_solution
                     else:
-                        better_solution = modif_true[0]
-    print(modif_true)
+                        if cpt_validity < nb_accept:
+                            better_solution = new_solution
+                            cpt_validity = cpt_validity + 1
+                            modif_false[0] = better_solution
+                            modif_false[1] = check_new_solution
+                        else:
+                            better_solution = modif_true[0]
+        print(modif_true)
 
     return new_solution
 
@@ -64,10 +67,10 @@ def modify_solution(solution, path_to_modify, modification):
     return sol
 
 
-evac, ark = ir.parse_instance('./Instances/sparse_10_30_3_2_I.full')
+evac, ark = ir.parse_instance('./Instances/medium_10_30_3_2_I.full')
 #print(chk.run_with_objective('./Instances/sparse_10_30_3_2_I.full',brn.borne_sup_solution(evac, ark) ))
-local_search('./Instances/sparse_10_30_3_2_I.full', evac, brn.borne_sup_solution(evac, ark))
+local_search('./Instances/medium_10_30_3_2_I.full', evac, ark, brn.borne_sup_solution(evac, ark))
 
 
 #evac, ark = ir.parse_instance('./Exemple/graphe-TD-sans-DL-data.txt')
-#local_search('./Exemple/graphe-TD-sans-DL-data.txt', evac, brn.borne_sup_solution(evac, ark))
+#local_search('./Exemple/graphe-TD-sans-DL-data.txt', evac, ark,  brn.borne_sup_solution(evac, ark))
